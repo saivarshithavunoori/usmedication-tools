fb8d5e7924af8bdd1ffde92311d4ab6faa8d5cdb
Sai Varshith Avunoori <mail@saivarshith.in>
2025-12-28 18:31:23 +0530

    Delete medication-alternative-checker.php

--- medication-alternative-checker.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Plugin Name: Medication Alternative & Side Effects Checker
- * Description: Provides medication alternatives, brand equivalents, and side effects information using reliable US health data sources.
- * Version: 1.0.0
- * Author: Shekel Tech
- * License: GPLv2 or later
- */
